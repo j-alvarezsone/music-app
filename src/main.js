3afd71676c2bf7f8651e37105fa7fc7b1a24596e
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-08 21:57:11 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import Music from './Music.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(Music).use(store).use(router).mount('#app');
