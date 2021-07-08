fd031917ec302139e1398919c85dd545f1fad1ba
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-08 23:50:21 +0200

    acreateApp assigned to variable

--- src/main.js
+++ src/main.js
@@ -5,4 +5,9 @@ import store from './store';
 import './assets/tailwind.css';
 import './assets/main.css';
 
-createApp(Music).use(store).use(router).mount('#app');
+const app = createApp(Music);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
