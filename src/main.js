026540edaa9558c7078e2907dc9ed9d15cdcc6ef
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-08 22:21:13 +0200

    installed tailwind

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import Music from './Music.vue';
 import router from './router';
 import store from './store';
+import './assets/tailwind.css';
 
 createApp(Music).use(store).use(router).mount('#app');
