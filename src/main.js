d66a39fa9769195c1ea5fd0436c008a989f8b624
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-08 23:04:40 +0200

    added main.css

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import Music from './Music.vue';
 import router from './router';
 import store from './store';
 import './assets/tailwind.css';
+import './assets/main.css';
 
 createApp(Music).use(store).use(router).mount('#app');
