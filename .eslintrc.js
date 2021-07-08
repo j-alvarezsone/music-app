cd4efa5303a9139b905a4944d6963d2125929293
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-08 23:03:37 +0200

    changed max-len to off

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
-    'max-len': ['error', { code: 110 }],
+    'max-len': 'off',
     'vue/comment-directive': 0,
   },
   overrides: [
