48d8cdabfef215ea8b4826219569bfb27845c7c0
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-13 15:43:04 +0200

    added createUserWithEmailAndPassword and usersCollection

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -157,6 +157,8 @@
   </vee-form>
 </template>
 <script>
+import { auth, usersCollection } from '@/includes/firebase';
+
 export default {
   name: 'RegisterForm',
   data() {
@@ -165,7 +167,7 @@ export default {
         name: 'required|min:3|max:100|alpha_spaces',
         email: 'required|min:3|max:100|email',
         age: 'required|min_value:18|max_value:100',
-        password: 'required|min:3|max:100',
+        password: 'required|min:6|max:100',
         confirm_password: 'password_mismatch:@password',
         country: 'required|country_excluded:Antarctica',
         tos: 'tos',
@@ -180,15 +182,39 @@ export default {
     };
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
+      let userCred = null;
+      try {
+        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password);
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error occurred. Please try again later';
+        return;
+      }
+
+      try {
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        });
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error occurred. Please try again later';
+        return;
+      }
+
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(values);
+      console.log(userCred);
     },
   },
 };
