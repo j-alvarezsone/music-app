0089eb25975d8f8a2a474e3b99ad1009c8521b71
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-27 10:59:18 +0200

    created dynamically registering modules

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -3,10 +3,17 @@ import { camelCase } from 'lodash';
 const requireModule = require.context('.', false, /\.js$/);
 const modules = {};
 
+const fileExclusions = ['./index.js', './dummy.js'];
+
 requireModule.keys().forEach((fileName) => {
-  if (fileName === './index.js' || fileName === './dummy.js') {
+  if (fileExclusions.some((x) => x === fileName)) {
     return;
   }
+
+  // requireModule.keys().forEach((fileName) => {
+  //   if (fileName === './index.js' || fileName === './dummy.js') {
+  //     return;
+  //   }
 
   const moduleConfig = requireModule(fileName);
   const moduleName = camelCase(fileName.replace(/(\.\/|\.js)/g, ''));
